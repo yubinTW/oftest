f8aed7afe17d06625e127fdfb65ab88e0b1723db
Macauley Cheng <macauley_cheng@accton.com>
2016-03-24 11:01:48 +0800

    use another way to send packet
    use another way to send packet

--- acctonUseDpctl/phase1.py
+++ acctonUseDpctl/phase1.py
@@ -15,14 +15,15 @@ class l3ucast_route(base_tests.SimpleDataPlane):
     Inject  eth 1/3 Tag2, SA000000112233, DA7072cf7cf3a3, SIP 192.168.1.100, DIP 192.168.2.2
     Output  eth 1/1 Tag3, SA 000004223355, DA 000004224466
 
-    ./dpctl tcp:192.168.1.1:6633 flow-mod table=0,cmd=add,prio=1 in_port=0/0xffff0000 goto:10
-    ./dpctl tcp:192.168.1.1:6633 flow-mod table=10,cmd=add,prio=101 in_port=3,vlan_vid=0x1002/0x1fff goto:20
-    ./dpctl tcp:192.168.1.1:6633 flow-mod table=20,cmd=add,prio=201 in_port=3,vlan_vid=2/0xfff,eth_dst=70:72:cf:7c:f3:a3,eth_type=0x0800 goto:30
-    ./dpctl tcp:192.168.1.1:6633 group-mod cmd=add,type=ind,group=0x30001 group=any,port=any,weight=0 output=1
-    ./dpctl tcp:192.168.1.1:6633 group-mod cmd=add,type=ind,group=0x20000003 group=any,port=any,weight=0 set_field=eth_src=00:00:04:22:33:55,set_field=eth_dst=00:00:04:22:44:66,set_field=vlan_vid=3,group=0x30001
-    ./dpctl tcp:192.168.1.1:6633 flow-mod table=30,cmd=add,prio=301 eth_type=0x0800,ip_dst=192.168.2.2/255.255.255.0 write:group=0x20000003 goto:60
+    ./dpctl tcp:192.168.2.1:6633 flow-mod table=0,cmd=add,prio=1 in_port=0/0xffff0000 goto:10
+    ./dpctl tcp:192.168.2.1:6633 flow-mod table=10,cmd=add,prio=101 in_port=1,vlan_vid=0x1002/0x1fff goto:20
+    ./dpctl tcp:192.168.2.1:6633 flow-mod table=20,cmd=add,prio=201 in_port=1,vlan_vid=2/0xfff,eth_dst=70:72:cf:7c:f3:a3,eth_type=0x0800 goto:30
+    ./dpctl tcp:192.168.2.1:6633 group-mod cmd=add,type=ind,group=0x30002 group=any,port=any,weight=0 output=2
+    ./dpctl tcp:192.168.2.1:6633 group-mod cmd=add,type=ind,group=0x20000003 group=any,port=any,weight=0 set_field=eth_src=00:00:04:22:33:55,set_field=eth_dst=00:00:04:22:44:66,set_field=vlan_vid=3,group=0x30002
+    ./dpctl tcp:192.168.2.1:6633 flow-mod table=30,cmd=add,prio=301 eth_type=0x0800,ip_dst=192.168.2.2/255.255.255.0 write:group=0x20000003 goto:60
     """
     def runTest(self):
+
         delete_all_flows(self.controller)
         delete_all_groups(self.controller)
 
@@ -34,26 +35,27 @@ class l3ucast_route(base_tests.SimpleDataPlane):
         apply_dpctl_mod(self, config, "meter-mod cmd=del,meter=0xffffffff")
         apply_dpctl_mod(self, config, "flow-mod table=0,cmd=add,prio=1 in_port=0/0xffff0000 goto:10")
         apply_dpctl_mod(self, config, "flow-mod table=10,cmd=add,prio=101 in_port="+str(input_port)+",vlan_vid=0x1002/0x1fff goto:20")
-        apply_dpctl_mod(self, config, "flow-mod table=20,cmd=add,prio=201 in_port="+str(input_port)+",eth_dst=70:72:cf:7c:f3:a3,eth_type=0x0800 goto:30")
+        apply_dpctl_mod(self, config, "flow-mod table=20,cmd=add,prio=201 in_port="+str(input_port)+",eth_dst=00:00:00:11:33:55,eth_type=0x0800,vlan_vid=2 goto:30")
         apply_dpctl_mod(self, config, "group-mod cmd=add,type=ind,group=0x3000"+str(output_port)+" group=any,port=any,weight=0 output="+str(output_port))
         apply_dpctl_mod(self, config, "group-mod cmd=add,type=ind,group=0x20000003 group=any,port=any,weight=0 set_field=eth_src=00:00:04:22:33:55,set_field=eth_dst=00:00:04:22:44:66,set_field=vlan_vid=3,group=0x3000"+str(output_port))
-        apply_dpctl_mod(self, config, "flow-mod table=30,cmd=add,prio=301 eth_type=0x0800,ip_dst=192.168.2.2/255.255.255.0 write:group=0x20000003 goto:60")
+        apply_dpctl_mod(self, config, "flow-mod table=30,cmd=add,prio=301 eth_type=0x0800,ip_dst=192.168.3.2/255.255.255.0 write:group=0x20000003 goto:60")
 
-        input_pkt = simple_packet(
-                '70 72 cf 7c f3 a3 00 00 00 11 22 33 81 00 00 02 '
-                '08 00 45 00 00 4e 04 d2 00 00 7f 06 b2 7b c0 a8 '
-                '01 0a c0 a8 02 02 00 03 00 06 00 01 f7 fa 00 00 '
-                '00 00 50 00 04 00 2f 5d 00 00 00 00 00 00 00 00 '
-                '00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 '
-                '00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00')
-
-        output_pkt = simple_packet(
-                '00 00 04 22 44 66 00 00 04 22 33 55 81 00 00 03 '
-                '08 00 45 00 00 4e 04 d2 00 00 7e 06 b3 7b c0 a8 '
-                '01 0a c0 a8 02 02 00 03 00 06 00 01 f7 fa 00 00 '
-                '00 00 50 00 04 00 2f 5d 00 00 00 00 00 00 00 00 '
-                '00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 '
-                '00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00')
+        input_pkt = simple_tcp_packet(pktlen=100,
+                                       eth_dst='00:00:00:11:33:55',
+                                       eth_src='00:00:00:11:22:33',
+                                       ip_src='192.168.5.10',
+                                       ip_dst='192.168.3.2',
+                                       ip_ttl=64,
+                                       vlan_vid=2,
+                                       dl_vlan_enable=True)
+        output_pkt = simple_tcp_packet(pktlen=100,
+                                       eth_dst='00:00:04:22:44:66',
+                                       eth_src='00:00:04:22:33:55',
+                                       ip_src='192.168.5.10',
+                                       ip_dst='192.168.3.2',
+                                       ip_ttl=63,
+                                       vlan_vid=3,
+                                       dl_vlan_enable=True)
 
         self.dataplane.send(input_port, str(input_pkt))
         verify_packet(self, str(output_pkt), output_port)
