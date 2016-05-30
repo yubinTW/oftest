d5be5d90ad31fee67cdadcabcf698ea1bc802328
wind-lai <wintinglai@gmail.com>
2016-05-30 14:37:11 +0800

    modify tx packet
    modify tx packet

--- acctonUseDpctl/conformance_test.py
+++ acctonUseDpctl/conformance_test.py
@@ -30,16 +30,17 @@ class conformance_test(base_tests.SimpleDataPlane):
 
         apply_dpctl_mod(self, config, "meter-mod cmd=del,meter=0xffffffff")
         apply_dpctl_mod(self, config, "flow-mod table=10,cmd=add,prio=101 in_port="+str(input_port)+",vlan_vid=0x1002/0x1fff goto:20")
+        apply_dpctl_mod(self, config, "flow-mod table=10,cmd=add,prio=101 in_port="+str(output_port)+",vlan_vid=0x1002/0x1fff goto:20")
         apply_dpctl_mod(self, config, "group-mod cmd=add,type=ind,group=0x2000"+str(output_port)+" group=any,port=any,weight=0 output="+str(output_port))
         apply_dpctl_mod(self, config, "flow-mod table=0,cmd=add,prio=301 eth_type=0x0800,in_port="+str(input_port)+",vlan_vid=2 apply:output="+str(output_port))
 
-        input_pkt = simple_packet(
-                '00 00 00 22 33 55 00 00 00 11 22 33 81 00 00 02 '
-                '08 00 45 00 00 4e 04 d2 00 00 7f 06 b2 7b c0 a8 '
-                '01 0a c0 a8 02 02 00 03 00 06 00 01 f7 fa 00 00 '
-                '00 00 50 00 04 00 2f 5d 00 00 00 00 00 00 00 00 '
-                '00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 '
-                '00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00')
+        input_pkt = simple_tcp_packet(pktlen=100,
+                                       eth_dst='00:00:00:11:33:55',
+                                       eth_src='00:00:00:11:22:33',
+                                       ip_src='192.168.5.10',
+                                       ip_dst='192.168.3.2',
+                                       vlan_vid=2,
+                                       dl_vlan_enable=True)
 
         output_pkt = input_pkt
 
