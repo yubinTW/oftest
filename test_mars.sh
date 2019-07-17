57cf3776c07b3157a8ea181c01756c0438567460
yubintw <yubin.cs05g@nctu.edu.tw>
2019-07-17 16:24:38 +0800

    Update test and command

--- test_mars.sh
+++ test_mars.sh
@@ -6,6 +6,6 @@ DEBUG=info
 
 echo "Start testing `date +%Y/%m/%d-%H:%M:%S`"
 
-sudo ./oft --host=${HOST} -i 1@${INTERFACE} --of-version=1.3 --mars=MARS --test-dir=mars --disable-ipv6 --debug=${DEBUG} account_test
+sudo ./oft --host=${HOST} -i 1@${INTERFACE} --of-version=1.3 --mars=MARS --test-dir=mars --disable-ipv6 --debug=${DEBUG} $@
 
 echo "End testing `date +%Y/%m/%d-%H:%M:%S`"
