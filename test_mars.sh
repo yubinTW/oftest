97ac646cefc880e062020aa56f57a0a7c0f32741
yubintw <yubin.cs05g@nctu.edu.tw>
2019-07-18 17:34:05 +0800

    Check dependencies and set parapeter with system virable

--- test_mars.sh
+++ test_mars.sh
@@ -1,11 +1,22 @@
 #! /bin/bash
 
-HOST=210.63.204.29
-INTERFACE=enp0s3
+HOST=${MARS_IP}
+INTERFACE=${OFTEST_INTERFACE}
 DEBUG=info
+
+# check dependencies
+package=`apt list --installed 2>/dev/null | grep scapy | awk 'BEGIN {FS="/"} {print $1}'`
+if [[ ${package} == *'scapy'* ]]; then
+  echo "scapy is installed"
+else
+  echo "scapy not found"
+  echo "install scapy"
+  sudo apt-get install -y scapy
+fi
 
 echo "Start testing `date +%Y/%m/%d-%H:%M:%S`"
 
+# run test in mars/
 sudo ./oft --host=${HOST} -i 1@${INTERFACE} --of-version=1.3 --mars=MARS --test-dir=mars --disable-ipv6 --debug=${DEBUG} $@
 
 echo "End testing `date +%Y/%m/%d-%H:%M:%S`"
