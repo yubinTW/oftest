75147eb7a01e7db943153ca47fffe52ca5b051fb
yubintw <yubin.cs05g@nctu.edu.tw>
2019-07-31 10:05:48 +0800

    Modify tenant API endpoint

--- mars/tenants_test.py
+++ mars/tenants_test.py
@@ -20,7 +20,7 @@ POST_HEADER = {'Authorization': AUTH_TOKEN, 'Content-Type': 'application/json'}
 class TenantsGetTest(base_tests.SimpleDataPlane):
     """
     Test tenant GET method
-        - /v1/tenants
+        - /v1/tenants/v1
     """
 
     def setUp(self):
@@ -37,32 +37,32 @@ class TenantsGetTest(base_tests.SimpleDataPlane):
 class TenantsAddNewTest(base_tests.SimpleDataPlane):
     """
     Test adding a new tenant and delete it
-      - POST v1/tenants
-      - DELETE v1/tenants/{tenant_id}
-      - GET v1/tenants
+      - POST v1/tenants/v1
+      - DELETE v1/tenants/v1/{tenant_name}
+      - GET v1/tenants/v1
     """
 
     def runTest(self):
-        tenant_id = 'testTenant' + str(int(time.time()))
+        tenant_name = 'testTenant' + str(int(time.time()))
         # add a tenant
-        payload = '{"id": "' + tenant_id + '"}'
+        payload = '{"name": "' + tenant_name + '", "type": "System"}'
         response = requests.post(
             URL+"v1/tenants/v1", headers=POST_HEADER, data=payload)
-        assert(response.status_code == 201)
+        assert(response.status_code == 200)
 
         # query tenants
         response = requests.get(URL + 'v1/tenants/v1', headers=GET_HEADER)
         assert(response.status_code == 200)
         found = False
         for t in response.json()['tenants']:
-            if t['id'] == tenant_id:
+            if t['name'] == tenant_name:
                 found = True
                 break
         assert(found)
 
         # delete test tenant
-        response = requests.delete(URL + 'v1/tenants/v1/{}'.format(tenant_id), headers=GET_HEADER)
-        assert(response.status_code == 204)
+        response = requests.delete(URL + 'v1/tenants/v1/{}'.format(tenant_name), headers=GET_HEADER)
+        assert(response.status_code == 200)
 
         # query and check
         response = requests.get(URL + 'v1/tenants/v1', headers=GET_HEADER)
@@ -70,7 +70,7 @@ class TenantsAddNewTest(base_tests.SimpleDataPlane):
         not_exist = True
         if len(response.json()) > 0:
             for t in response.json()['tenants']:
-                if t['id'] == tenant_id:
+                if t['name'] == tenant_name:
                     not_exist = False
                     break
         assert(not_exist)
